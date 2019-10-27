8366fb69d1d293e108c0f13978faa46fd0e43335
unknown <michaellang2525@yahoo.com>
2019-10-27 18:07:12 -0400

    game logic built and working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,23 @@
 
 //creates the array of word choices for the computer
-var computerChoices = ["jeter"]
+var computerChoices = ["flower","puzzle","agenda"]
 //creates an array container for the user's guesses 
 var userGuessAll = []
 
-var wins = document.getElementById("txt-wins");
+var frmwin = document.getElementById("txt-wins");
+var frmloss = document.getElementById("txt-loss");
 var guessesRemaining = document.getElementById("txt-gs-rem");
 var guessesAll = document.getElementById("txt-gs-all");
 var currentword = document.getElementById("txt-word")
 var Buildword = ""
+var wins = 0
 
 //chooses a random selection of the computerChoices array created earlier
 
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+
 
 document.onkeyup = function(event) {
     
@@ -21,15 +26,15 @@ document.onkeyup = function(event) {
         computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     }
 
-    console.log("Computer Guesses: " + computerGuess)
-    console.log("Computer Guess is " + computerGuess.length + " characters long")
+    console.log("Computer Guesses: " + computerGuess);
+    console.log("Computer Guess is " + computerGuess.length + " characters long");
     var userGuess = event.key.toLowerCase();
-    console.log("User Guesses: " + userGuess)
+    console.log("User Guesses: " + userGuess);
 
 
         //determines if the user guess already exists in the array of guesses 
         if(userGuessAll.indexOf(userGuess) == -1){
-            
+            guessesRemaining.innerText = parseInt(guessesRemaining.innerText) - 1
                 //adds the users guess to the userGuessAll array 
                 userGuessAll.push(userGuess)
             
@@ -42,27 +47,37 @@ document.onkeyup = function(event) {
                         
                     }
                     else{
-                        Buildword = Buildword + "_"
+                        Buildword = Buildword + " _ ";
                     }
                 }
+
+                //updates the txt-word to however many letters the user has gotten
                 currentword.innerText = Buildword
 
+
+
             //determines if they are any guesses remaining 
-            if(parseInt(guessesRemaining.innerText) != 1){
+            if(parseInt(guessesRemaining.innerText) != 0){
 
-                //subtracts the guesses remaining by 1 each time a key is pressed
-                guessesRemaining.innerText = parseInt(guessesRemaining.innerText) - 1
-
-                //shows the user all letters they have guessed so far 
+                //checks to see if the user guesses equal the value of the computer guess 
+                if(Buildword == computerGuess){
+                    wins = wins + 1;
+                    guessesRemaining.innerText = 13;
+                    currentword.innerText = "_  _  _  _  _  _";
+                    userGuessAll = [];
+                }
+                //updates the letters guessed on the page to the userGuessAll array
                 guessesAll.innerText = userGuessAll
             }
             //resets values and determines if there was a win when the guesses are gone
             else {
-                userGuessAll = []
-                guessesAll.innerText = ""
-                guessesRemaining.innerText = 13
-                computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+                userGuessAll = [];
+                guessesAll.innerText = "";
+                guessesRemaining.innerText = 13;
             }
-        }       
+        }   
+        
+    frmwin.innerText = wins
 
 }
